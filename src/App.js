10b5231d36c8b7c1e21b30b68d9c633e2219509f
Eric Weiler <ejweiler@gmail.com>
2017-03-13 09:25:11 -0500

    initial commit, grid and reducer working. tests and packaing for export in progress.

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import Grid from './Grid'
+import MyState from './myState'
+import store from './ReduxStore'
+
+import './App.css'
+import 'ag-grid/dist/styles/ag-grid.css'
+import 'ag-grid/dist/styles/theme-fresh.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div style={{
+            height: '100%',
+            overflow: 'hidden'
+          }}>
+          <Grid />
+          <div className="state-view" >
+
+            <MyState />
+
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/ReduxStore.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, createStore } from 'redux'
+import { combineReducers } from 'redux'
+import thunk from "redux-thunk"
+import promise from "redux-promise-middleware"
+import gridReducer from "./gridReducer.js"
+
+let mw = [promise(), thunk]
+const middleware = applyMiddleware(...mw)
+
+const reducer = combineReducers({
+  grid: gridReducer
+})
+
+var store = createStore(reducer, middleware)
+
+export default store
